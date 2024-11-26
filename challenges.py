7a4616d09b7c38eebd345dc47924bb01ed0dbfd2
KHAI PHAN <hphan16@my.bcit.ca>
2024-11-26 14:39:59 -0800

    Edit try-except block for hostage_rescue

--- challenges.py
+++ challenges.py
@@ -88,8 +88,8 @@ def hostage_rescue(character):
             user_guess = input('Guess the number of hostage between 1 and 5: ')
         except ValueError:
             print("You must enter a positive integer!")
-            if user_guess not in options:
-                print("Invalid input. Please enter an integer between 1 and 5.")
+        if user_guess not in options:
+            print("Invalid input. Please enter an integer between 1 and 5.")
     if int(user_guess) == hostage_roll:
         character['XP'] = min(600, character['XP'] + 100)
         print(f'Correct! You just rescued the victims and gained 100 XP (max 600 XP)! '
