ade76123d516d1e9b38659ed987e4f8f68f9874d
KHAI PHAN <hphan16@my.bcit.ca>
2024-11-27 23:41:56 -0800

    Edit unit test for is_alive

--- test_is_alive.py
+++ test_is_alive.py
@@ -5,8 +5,8 @@ from game import is_alive
 class Test(TestCase):
 
     def test_is_alive_0_HP(self):
-        character = {'HP': 0}
-        actual = is_alive(character)
+        char = {'HP': 0}
+        actual = is_alive(char)
         expected = False
         self.assertEqual(expected, actual)
 
@@ -17,19 +17,19 @@ class Test(TestCase):
         self.assertEqual(expected, actual)
 
     def test_is_alive_2_HP(self):
-        character = {'HP': 2}
-        actual = is_alive(character)
+        char = {'HP': 2}
+        actual = is_alive(char)
         expected = True
         self.assertEqual(expected, actual)
 
     def test_is_alive_3_HP(self):
-        character = {'HP': 3}
-        actual = is_alive(character)
+        char = {'HP': 3}
+        actual = is_alive(char)
         expected = True
         self.assertEqual(expected, actual)
 
     def test_is_alive_10_HP(self):
-        character = {'HP': 10}
-        actual = is_alive(character)
+        char = {'HP': 10}
+        actual = is_alive(char)
         expected = True
         self.assertEqual(expected, actual)
