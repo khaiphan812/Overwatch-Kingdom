e41b84d3742e738e8954d2413ecb091a5f9bb59d
KHAI PHAN <hphan16@my.bcit.ca>
2024-12-01 16:53:35 -0800

    Add text colors for game

--- game.py
+++ game.py
@@ -3,6 +3,7 @@ Khai Phan
 Overwatch Kingdom
 """
 import time
+from colorama import Fore, init
 from board import make_board
 from board import describe_current_location
 from characters import make_character
@@ -22,19 +23,20 @@ def game():
     """
     Drive the game.
     """
-    rows = 8
-    columns = 8
+    init()
+    rows = 10
+    columns = 10
     board = make_board(rows, columns)
     character = make_character()
     doom = make_final_boss()
     time.sleep(1)
-    print("Story:\n"
-          "Overwatch Kingdom was once a peaceful land with bliss and joy.\n"
+    print(Fore.YELLOW + "Story:\n" +
+          Fore.RESET + "Overwatch Kingdom was once a peaceful land with bliss and joy.\n"
           "Then Master Doom came and disrupted the harmony of the kingdom.\n"
           "You are Reinhardt Wilhelm, a watchful guardian who vowed to protect the kingdom at all costs.")
     time.sleep(1)
-    print("Mission:\n"
-          "Navigate through the map and overcome challenges on the way.\n"
+    print(Fore.YELLOW + "Mission:\n" +
+          Fore.RESET + "Navigate through the map and overcome challenges on the way.\n"
           "Every time you win a challenge, you'll gain 100 XP for levelling up.\n"
           "The milestones to level up are 300 XP for level 2 and 600 XP (max) for level 3.\n"
           "You are now at Level 1 with 0 XP, 5 HP, and holding a Dragon Blade as your weapon.\n"
@@ -63,11 +65,11 @@ def game():
                 if character['HP'] > 0:
                     mission_complete = True
         else:
-            print("Sorry you can't go in that direction.")
+            print(Fore.RED + "Sorry you can't go in that direction.")
     if not is_alive(character):
-        print("Sorry, you died. You will need another effort to save the Overwatch Kingdom.")
+        print(Fore.RED + "Sorry, you died. You will need another effort to save the Overwatch Kingdom.")
         time.sleep(1)
-        print("""
+        print(Fore.RED + """
             ██╗    ███╗██╗███████╗███████╗██╗ ██████╗ ███╗   ██╗
             ████╗ ████║██║██╔════╝██╔════╝██║██╔═══██╗████╗  ██║
             ██╔████╔██║██║███████╗███████╗██║██║   ██║██╔██╗ ██║
@@ -83,9 +85,9 @@ def game():
             ╚═╝     ╚═╝  ╚═╝╚═╝╚══════╝╚══════╝╚═════╝ ╚═╝ 
               """)
     else:
-        print("Congratulations! Peace has finally returned to Overwatch Kingdom!")
+        print(Fore.YELLOW + "Congratulations! Peace has finally returned to Overwatch Kingdom!")
         time.sleep(1)
-        print("""
+        print(Fore.YELLOW + """
             ███╗   ███╗██╗███████╗███████╗██╗ ██████╗ ███╗   ██╗                    
             ████╗ ████║██║██╔════╝██╔════╝██║██╔═══██╗████╗  ██║                    
             ██╔████╔██║██║███████╗███████╗██║██║   ██║██╔██╗ ██║                    
